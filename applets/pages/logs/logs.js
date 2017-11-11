45e1ca013c43def6c43f70ebbf95048c00fbaef0
ttjslbz <33570967+chenh1-inkfly@users.noreply.github.com>
2017-11-11 19:47:59 +0800

    demo weixin applet
    weixin applet initialize

---
+++ applets/pages/logs/logs.js
@@ -0,0 +1,13 @@
+// pages/logs/logs.js
+Page({
+  data: {
+    logs: []
+  },
+  onLoad: function () {
+    this.setData({
+      logs: (wx.getStorageSync('logs') || []).map(log => {
+        return util.formatTime(new Date(log))
+      })
+    })
+  }
+})
