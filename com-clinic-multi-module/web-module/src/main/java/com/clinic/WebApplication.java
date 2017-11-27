dd6079deb87b289ff032908dc819abd7c4c3dac3
longpeng05 <longpeng05@meituan.com>
2017-11-27 19:04:36 +0800

    add code

--- com-clinic-multi-module/web-module/src/main/java/com/clinic/WebApplication.java
+++ com-clinic-multi-module/web-module/src/main/java/com/clinic/WebApplication.java
@@ -13,15 +13,7 @@ import com.alibaba.dubbo.config.RegistryConfig;
 @SpringBootApplication
 @ImportResource("classpath:applicationContext.xml")
 public class WebApplication {
-	@Bean(name="defaultConsumer")
-    public ConsumerConfig providerConfig(ApplicationConfig applicationConfig, RegistryConfig registryConfig) {
-        ConsumerConfig providerConfig = new ConsumerConfig();
-        // 手动注入，不再由spring容器去根据类名
-        // (com.alibaba.dubbo.config.ApplicationConfig)去获取对应的值
-        providerConfig.setApplication(applicationConfig);
-        providerConfig.setRegistry(registryConfig);
-        return providerConfig;
-    }
+	
 	public static void main(String[] args) {
 		new SpringApplicationBuilder()
 			.bannerMode(Banner.Mode.CONSOLE)
